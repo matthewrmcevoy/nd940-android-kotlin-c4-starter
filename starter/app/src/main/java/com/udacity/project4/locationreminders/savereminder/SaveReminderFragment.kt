8721f47ebc37af9d182617630c17de3fd0c89610
matthewrmcevoy <matthewrmcevoy@gmail.com>
2022-10-01 12:40:26 -0400

    Commit 4; Added functionality: Setting POI Location for geofence. Saving the geofence. Enabling notification when entering geofence.
    TO DO: Add Reminder Details...
    then: Add testing

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,21 +1,51 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
+import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import com.udacity.project4.locationreminders.savereminder.selectreminderlocation.SelectLocationFragment
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
+
+const val ACTION_GEOFENCE_EVENT = "GEOFENCE_EVENT"
+private const val REQUEST_FOREGROUND_ONLY = 123
+private const val REQUEST_FOREBACK_PERM = 321
+private const val REQUEST_LOCATION_ON = 55
 
 class SaveReminderFragment : BaseFragment() {
     //Get the view model this time as a single to be shared with the another fragment
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSaveReminderBinding
+    private lateinit var geofencingClient: GeofencingClient
+    private lateinit var newGeofenceReminder: ReminderDataItem
+
+
+    private val runningQOrLater = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    private val geofencePendingIntent: PendingIntent by lazy{
+        val intent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+        intent.action = ACTION_GEOFENCE_EVENT
+        PendingIntent.getBroadcast(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +57,7 @@ class SaveReminderFragment : BaseFragment() {
         setDisplayHomeAsUpEnabled(true)
 
         binding.viewModel = _viewModel
+        geofencingClient = LocationServices.getGeofencingClient(requireContext())
 
         return binding.root
     }
@@ -40,16 +71,130 @@ class SaveReminderFragment : BaseFragment() {
                 NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
         }
 
+
         binding.saveReminder.setOnClickListener {
             val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription
+            val description = _viewModel.reminderDescription.value
             val location = _viewModel.reminderSelectedLocationStr.value
-            val latitude = _viewModel.latitude
+            val latitude = _viewModel.latitude.value
             val longitude = _viewModel.longitude.value
 
-//            TODO: use the user entered reminder details to:
-//             1) add a geofencing request
-//             2) save the reminder to the local db
+            newGeofenceReminder = ReminderDataItem(
+                title, description, location, latitude, longitude
+            )
+           //Must enable geofencing permissions prior to saving and setting geofencing so check permissions on save
+            checkPermsStartGeofencing()
+
+        }
+    }
+
+    private fun checkPermsStartGeofencing(){
+        if(foregroundAndBackgroundLocationsPermsApproved()){
+            checkLocationSettingsStartGeofence()
+        }
+        else{
+            requestForeBackgroundLocationPerms()
+        }
+    }
+    private fun foregroundAndBackgroundLocationsPermsApproved() : Boolean {
+        //if running Q or later, require both foreground and background permissions
+        return if(runningQOrLater) {
+            (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED)
+        // otherwise, only require foreground location
+        }else{
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+    private fun requestForeBackgroundLocationPerms(){
+        if(foregroundAndBackgroundLocationsPermsApproved()){
+            return
+        }else if(runningQOrLater){
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            requestPermissions(permissions, REQUEST_FOREBACK_PERM)
+        }else{
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            requestPermissions(permissions, REQUEST_FOREGROUND_ONLY)
+        }
+    }
+
+    private fun checkLocationSettingsStartGeofence(resolve: Boolean = true){
+        val locationRequest = LocationRequest.create().apply{
+            priority = LocationRequest.PRIORITY_LOW_POWER
+        }
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val settingsClient = LocationServices.getSettingsClient(requireContext())
+        val locationSettingsResponseTask = settingsClient.checkLocationSettings(builder.build())
+
+        locationSettingsResponseTask.addOnCompleteListener {
+            if(it.isSuccessful){
+                addNewGeofence()
+            }
+        }
+        locationSettingsResponseTask.addOnFailureListener { exception ->
+            if(exception is ResolvableApiException && resolve){
+                try{
+                    activity?.let{
+                        exception.startResolutionForResult(it, REQUEST_LOCATION_ON)
+                    }
+                }catch(sendEx: IntentSender.SendIntentException){
+                    Log.i("SaveRemFrag","Could not resolve location settings " + sendEx.message)
+                }
+            }else{
+                Snackbar.make(this.requireView(),R.string.location_required_error, Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok){
+                    checkLocationSettingsStartGeofence()
+                }.show()
+            }
+        }
+    }
+
+    private fun addNewGeofence(){
+        if(::newGeofenceReminder.isInitialized){
+            if(_viewModel.validateEnteredData(newGeofenceReminder)){
+                _viewModel.validateAndSaveReminder(newGeofenceReminder)
+                val GEOFENCE_RADIUS = 100f
+                val geofence = Geofence.Builder()
+                    .setRequestId(newGeofenceReminder.id)
+                    .setCircularRegion(newGeofenceReminder.latitude!!,newGeofenceReminder.longitude!!,GEOFENCE_RADIUS)
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                    .build()
+
+                Log.i("saveRemFrag","newGeofenceReminderID is ${newGeofenceReminder.id}")
+
+                val geofenceRequest = GeofencingRequest.Builder()
+                    .setInitialTrigger(Geofence.GEOFENCE_TRANSITION_ENTER)
+                    .addGeofence(geofence)
+                    .build()
+
+                geofencingClient.addGeofences(geofenceRequest, geofencePendingIntent).run {
+                    addOnSuccessListener {
+                       // Snackbar.make(requireView(),"Geofence Added!",Snackbar.LENGTH_SHORT).show()
+                        //Toast.makeText(requireContext(),"Geofence Added!", Toast.LENGTH_LONG).show()
+                        Log.i("saveRemFrag","GEOFENCE ADDED")
+                        //_viewModel.navigationCommand.value=NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToReminderListFragment())
+                    }
+                    addOnFailureListener {
+                        //Snackbar.make(requireView(),R.string.geofences_not_added, Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok){null}.show()
+                        Log.i("saveRemFrag","Geofence could not be added")
+                        if(it.message != null){
+                            //Toast.makeText(requireContext(),it.message, Toast.LENGTH_LONG).show()
+                            Log.i("saveRemFrag","GEOFENCE NOT ADDED due to: " + it.message)
+                        }
+                    }
+                }
+                _viewModel.onClear()
+                _viewModel.navigationCommand.value=NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToReminderListFragment())
+            }
         }
     }
 
--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -7,11 +7,14 @@ import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
@@ -28,12 +31,19 @@ import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
+import java.util.*
 
-class SelectLocationFragment : BaseFragment() {
+private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var poiMarker: Marker
+    lateinit var savedLocation: LatLng
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -47,22 +57,117 @@ class SelectLocationFragment : BaseFragment() {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
 //        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
+        binding.saveLocBttn.setOnClickListener {
+            onLocationSelected()
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
-
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         return binding.root
     }
 
+    override fun onMapReady(googleMap: GoogleMap){
+        map= googleMap
+        var curLat = 0.00
+        var curLong = 0.00
+        val mapZoomLvl = 16f
+        enableMyLocation()
+        if(map.isMyLocationEnabled){
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                curLat = location.latitude
+                curLong = location.longitude
+                val curLoc = LatLng(curLat, curLong)
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(curLoc, mapZoomLvl))
+                }else{
+                    Log.i("SaveLocFrag","location came up null")
+                }
+            }
+        }else{
+            enableMyLocation()
+        }
+        setPoiClick(map)
+//        map.setOnMarkerDragListener(object: GoogleMap.OnMarkerDragListener{
+//            override fun onMarkerDragStart(p0: Marker?) {
+//                marker.position
+//            }
+//
+//            override fun onMarkerDragEnd(p0: Marker?) {
+//                if (p0 != null) {
+//                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(p0.position, 16.0f))
+//                    marker.position = p0.position
+//                }
+//            }
+//
+//            override fun onMarkerDrag(p0: Marker?) {
+//                marker.position = p0!!.position
+//            }
+//        })
+        //val homeLoc = LatLng(homeLat, homeLong)
+    }
+
+//    private fun setMapLongClick(map: GoogleMap){
+//        map.setOnMapLongClickListener { location ->
+//
+//
+//            val snippet = String.format(Locale.getDefault(), "Lat: %1$.5f, Long: %2$.5f", location.latitude, location.longitude)
+//
+//            marker = map.addMarker(
+//                MarkerOptions()
+//                    .position(location)
+//                    .title(getString(R.string.dropped_pin))
+//                    .snippet(snippet)
+//                    .draggable(true)
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)))
+//
+////            if(marker.position == location) {
+////                marker.remove()
+////            }
+//        }
+//    }
+    private fun setPoiClick(map: GoogleMap){
+        map.setOnPoiClickListener { poi ->
+            //val snippet = String.format(Locale.getDefault(), "LatLng: %1$.5f", poi.latLng)
+            map.clear()
+            poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+                    //.snippet(snippet)
+            )
+            poiMarker?.showInfoWindow()
+            Log.i("loc","$poiMarker")
+        }
+    }
+
+
+    private fun enableMyLocation(){
+        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            map.isMyLocationEnabled = true
+            return
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(),arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_TURN_DEVICE_LOCATION_ON)
+        }
+    }
+
     private fun onLocationSelected() {
-        //        TODO: When the user confirms on the selected location,
-        //         send back the selected location details to the view model
-        //         and navigate back to the previous fragment to save the reminder and add the geofence
+        if(::poiMarker.isInitialized) {
+            Log.i("saveLoc", "${poiMarker.position}")
+            savedLocation = poiMarker.position
+            _viewModel.reminderSelectedLocationStr.value = poiMarker.title
+            _viewModel.latitude.value = poiMarker.position.latitude
+            _viewModel.longitude.value = poiMarker.position.longitude
+            findNavController().navigate(SelectLocationFragmentDirections.actionSelectLocationFragmentToSaveReminderFragment())
+        }else{
+            Snackbar.make(this.requireView(),R.string.select_location, Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok){
+                null
+            }.show()
+        }
+
     }
 
 
@@ -71,17 +176,20 @@ class SelectLocationFragment : BaseFragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
